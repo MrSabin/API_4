40143522e39c988a60de2d88d97f4148e1480a00
MrSabin <sabin.21011986@gmail.com>
2022-06-26 15:33:28 +0300

    Added image_download() function
    Moved image downloading logic to image_download() function

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import requests
 from pathlib import Path
 
 
-Path("./images").mkdir(parents=True, exist_ok=True)
-filename = "./images/hubble.jpg"
+def image_download(url, path):
+    Path(path).mkdir(parents=True, exist_ok=True)
+    filename = f"{path}hubble.jpg"
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-
-response = requests.get(url)
-print(response)
-response.raise_for_status()
-
-with open(filename, 'wb') as file:
-    file.write(response.content)
+path = "./images/"
+image_download(url, path)
