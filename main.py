ce0998b234be13604c9fb0efa37278fa08c05f09
MrSabin <sabin.21011986@gmail.com>
2022-06-26 18:27:58 +0300

    Downloaded images via SpaceX API

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 from pathlib import Path
 
 
-def image_download(url, path):
+def image_download(url, path, name):
     Path(path).mkdir(parents=True, exist_ok=True)
-    filename = f"{path}hubble.jpg"
+    filename = f"{path}{name}"
 
     response = requests.get(url)
     response.raise_for_status()
@@ -19,4 +19,8 @@ response = requests.get(url, params=payload)
 response.raise_for_status()
 answer = response.json()
 links = answer[0]['links']['flickr_images']
-print(links)
+
+for number, link in enumerate(links):
+    path = './images/'
+    name = f'spacex_{number}'
+    image_download(link, path, name)
