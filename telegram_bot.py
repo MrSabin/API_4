0a5323ebe6d4fc235b121a500629eeb465b8b252
MrSabin <sabin.21011986@gmail.com>
2022-07-04 19:48:15 +0300

    Published text to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,4 +6,4 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.environ["TELEGRAM_TOKEN"]
 bot = telegram.Bot(token=token)
-print(bot.get_me())
+bot.send_message(text='Hello world!', chat_id=-1001522793852)
