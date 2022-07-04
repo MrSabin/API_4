fdc47fe27efed2fe427e1ff24fe72ea8928aab52
MrSabin <sabin.21011986@gmail.com>
2022-07-04 19:57:32 +0300

    Published photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,4 +6,5 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.environ["TELEGRAM_TOKEN"]
 bot = telegram.Bot(token=token)
-bot.send_message(text='Hello world!', chat_id=-1001522793852)
+chat_id = -1001522793852
+bot.send_document(chat_id=chat_id, document=open('images/test.jpg', 'rb'))
