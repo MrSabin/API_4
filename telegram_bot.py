1a0f8408f0869622314326d38106762b38e74dd9
MrSabin <sabin.21011986@gmail.com>
2022-07-05 21:53:50 +0300

    Minor changes
    Moved chat_id to .env file. Added bot_send_photo func to use from other script

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.environ["TELEGRAM_TOKEN"]
+chat_id = os.environ["CHAT_ID"]
 bot = telegram.Bot(token=token)
-chat_id = -1001522793852
-bot.send_document(chat_id=chat_id, document=open('images/test.jpg', 'rb'))
+
+
+def bot_send_photo(image_path):
+    bot.send_document(chat_id=chat_id, document=open(image_path, 'rb'))
