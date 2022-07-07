342c7986a07df0879d6f3ca131ab1c7fb6cbef22
MrSabin <sabin.21011986@gmail.com>
2022-07-07 19:45:29 +0300

    Moved code into def main()
    Created new download_spacex_photo() function

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -3,18 +3,28 @@ import argparse
 import requests
 from download import download_image
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--id', help='ID запуска')
-args = parser.parse_args()
-if args.id:
-    url = f"https://api.spacexdata.com/v4/launches/{args.id}"
-else:
-    url = "https://api.spacexdata.com/v4/launches/latest"
-response = requests.get(url)
-response.raise_for_status()
-return_object = response.json()
-links = return_object['links']['flickr']['original']
-for number, link in enumerate(links):
-    path = './images/'
-    name = f'spacex_{number}'
-    download_image(link, path, name)
+
+def download_spacex_photo(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return_object = response.json()
+    links = return_object['links']['flickr']['original']
+    for number, link in enumerate(links):
+        path = './images/'
+        name = f'spacex_{number}'
+        download_image(link, path, name)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--id', help='ID запуска')
+    args = parser.parse_args()
+    if args.id:
+        url = f"https://api.spacexdata.com/v4/launches/{args.id}"
+    else:
+        url = "https://api.spacexdata.com/v4/launches/latest"
+    download_spacex_photo(url)
+
+
+if __name__ == '__main__':
+    main()
