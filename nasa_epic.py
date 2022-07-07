16a5a8267e162ed3a94cbcd4f5643b2a21ac3449
MrSabin <sabin.21011986@gmail.com>
2022-07-07 19:47:27 +0300

    Wrapped code into def main()
    Created new download_nasa_epic() function

--- nasa_epic.py
+++ nasa_epic.py
@@ -4,22 +4,32 @@ import requests
 from dotenv import load_dotenv
 from download import download_image
 
-load_dotenv()
-token = os.environ["NASA_TOKEN"]
-api_url = 'https://api.nasa.gov/EPIC/api/natural/images'
-payload = {'api_key': token}
-response = requests.get(api_url, params=payload)
-response.raise_for_status()
-return_object = response.json()
-image_urls = []
-for entry in return_object:
-    name = entry.get('image')
-    date = entry.get('date')
-    splitted_date, splitted_time = date.split(' ')
-    year, month, day = splitted_date.split('-')
-    archive_url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{name}.png'
-    image_urls.append(archive_url)
-for number, link in enumerate(image_urls):
-    path = './images/'
-    name = f'nasa_epic_{number}'
-    download_image(link, path, name, token)
+
+def download_nasa_epic(token):
+    api_url = 'https://api.nasa.gov/EPIC/api/natural/images'
+    payload = {'api_key': token}
+    response = requests.get(api_url, params=payload)
+    response.raise_for_status()
+    return_object = response.json()
+    image_urls = []
+    for entry in return_object:
+        name = entry.get('image')
+        date = entry.get('date')
+        splitted_date, splitted_time = date.split(' ')
+        year, month, day = splitted_date.split('-')
+        archive_url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{name}.png'
+        image_urls.append(archive_url)
+    for number, link in enumerate(image_urls):
+        path = './images/'
+        name = f'nasa_epic_{number}'
+        download_image(link, path, name, token)
+
+
+def main():
+    load_dotenv()
+    token = os.environ["NASA_TOKEN"]
+    download_nasa_epic(token)
+
+
+if __name__ == '__main__':
+    main()
