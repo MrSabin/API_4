85f660343f786b4f06a1fa7708345280603d10a0
MrSabin <sabin.21011986@gmail.com>
2022-07-07 20:12:04 +0300

    Splitted long line to two vars

--- nasa_epic.py
+++ nasa_epic.py
@@ -17,8 +17,9 @@ def download_nasa_epic(token):
         date = entry['date']
         splitted_date, splitted_time = date.split(' ')
         year, month, day = splitted_date.split('-')
-        archive_url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{name}.png'
-        image_urls.append(archive_url)
+        archive_url = 'https://api.nasa.gov/EPIC/archive/natural'
+        image_url = f'{archive_url}/{year}/{month}/{day}/png/{name}.png'
+        image_urls.append(image_url)
     for number, link in enumerate(image_urls):
         path = './images/'
         name = f'nasa_epic_{number}'
