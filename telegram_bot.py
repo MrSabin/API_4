f40db98df3e873a632ac69d689f482ca764978fb
MrSabin <sabin.21011986@gmail.com>
2022-07-17 21:44:53 +0300

    Added try/except block to avoid crash on net loss

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,14 @@
+import time
+
 import telegram
 
 
 def send_photo(image_path, token, chat_id):
     bot = telegram.Bot(token=token)
     with open(image_path, 'rb') as photo:
-        bot.send_document(chat_id=chat_id, document=photo)
+        try:
+            bot.send_document(chat_id=chat_id, document=photo)
+        except telegram.error.NetworkError:
+            print("Network error occured. Retrying in 10 seconds...")
+            time.sleep(10)
+            send_photo(image_path, token, chat_id)
